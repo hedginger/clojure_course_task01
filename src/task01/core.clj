0d20dda56152e4fec95ad7efdf03743f659fe4b7
hedginger <yanushkevicheg@gmail.com>
2013-03-21 03:21:28 +0200

    ITA Clojure course, task #1

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,6 +2,25 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
+; don't see how to check the name of the element is possible in this situation
+;(defn isLinkTag? [arg1]
+;  (and (vector? arg1) (= (first arg1) :a)))
+
+(defn extractLinksFromVector [ vect ] ;vector represents an html tag
+  (let [attrMap (second vect)] ; attribute map
+    (if (empty? vect)
+      []
+      ;check if a "class" attribute with the value "r" exist 
+      (if  (= (get attrMap :class) "r")
+        ;true
+        ;seek for a link tag (presumably one, the first children tag) and return "href" attribute
+        [(get (second (get vect 2)) :href)]
+        ;false
+        (reduce into [] (concat (map extractLinksFromVector  (filter vector? vect) )))
+      );end if
+    );end if
+  );end let
+);end defn
 
 (defn get-links []
 " 1) Find all elements containing {:class \"r\"}.
@@ -20,8 +39,8 @@ The link from the example above is 'https://github.com/clojure/clojure'.
 
 Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
 "
-  (let [data (parse "clojure_google.html")]
-    nil))
+  (let [data (parse "clojure_google.html") result []]
+    (extractLinksFromVector data)))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
